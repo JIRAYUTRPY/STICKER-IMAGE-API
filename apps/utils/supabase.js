5ff30c63c0fa7bc896d7ebe2ef121ca02dfc784a
jirayut <jirayutrpy@hotmail.com>
2024-01-25 22:21:31 +0700

    feat:test deploy

--- apps/utils/supabase.js
+++ apps/utils/supabase.js
@@ -2,5 +2,7 @@ import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 const supabaseUrl = "https://uamabakrtljqfptzfxje.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhbWFiYWtydGxqcWZwdHpmeGplIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNTQ5MDEwMiwiZXhwIjoyMDIxMDY2MTAyfQ.aydEgKA51C3G6gcpQ5lUgAFg2S4a0ZNo1hS38bbXsU";
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
